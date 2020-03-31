71e2bc69703dfeda98214a0f1fa6e0b1b98415b3
lidorco <lidorco@outlook.com>
2020-03-31 20:09:19 +0300

    Add raw byte in middle of function. Put functions in specific section

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@ struct pic_globals {
     void (*log_function)(char*, unsigned int);
 };
 
-
+__attribute__ ((section(".pic_code")))
 void logger(char* msg, unsigned int msg_length) {
 
     // push write parameters
@@ -18,14 +18,33 @@ void logger(char* msg, unsigned int msg_length) {
 }
 
 
-int main(){
+__attribute__ ((section(".pic_code")))
+__attribute__((fastcall))
+void asd_string(){
 
+    asm(".byte 0x61");
+    asm(".byte 0x62");
+    asm(".byte 0x63");
+
+}
+
+
+__attribute__ ((section(".pic_code")))
+void entry_point()
+{
     struct pic_globals global;
 
     global.log_function = logger;
 
     global.log_function("asd\n", 4);
 
+    asd_string();
+}
+
+
+int main() {
+
+    entry_point();
     return 0;
 }
 
