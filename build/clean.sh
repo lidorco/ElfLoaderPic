1259193c7c546006334d4cd66af49e150d8fceff
lidorco <lidorco@outlook.com>
2021-08-21 23:37:36 +0300

    Using cmake

---
+++ build/clean.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+rm -rf CMakeCache.txt CMakeFiles/ cmake_install.cmake Makefile shellcode.elf shellcode.pic
+rm -rf tests/CMakeCache.txt tests/CMakeFiles/ tests/cmake_install.cmake tests/Makefile tests/pic_runner.elf
